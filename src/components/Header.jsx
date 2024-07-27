a4ae0c1b44992a1e48bb01e1746cdbd98b077d8f
tien <tienvn998@gmail.com>
2024-07-27 21:21:34 +0700

    add SearchForm

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,37 +4,39 @@ import { faBed, faCalendar, faFemale } from "@fortawesome/free-solid-svg-icons";
 
 export default function Header() {
     return (
-        <div className="bg-main-color text-white pt-10 pb-20 relative">
+        <div className="bg-main-color text-white pt-10 pb-20 relative px-5 md:px-7">
             <div className="container mx-auto flex flex-col gap-8">
                 <h2 className="text-3xl font-semibold">A lifetime of discounts? It's Genius.</h2>
                 <p>Get rewared for your travels - unlocking instant saving of 10% or more with a free account</p>
                 <div>
                     <Button label='Sign in/Register' className='bg-blue-500 text-white px-3 py-4' />
                 </div>
-                <Form />
+                <SearchForm />
             </div>
         </div>
     )
 }
 
-function Form() {
+function SearchForm() {
     const onSubmit = e => {
         e.preventDefault();
     }
+
     return (
-        <form onSubmit={onSubmit} className='fixed absolute  translate-x-1/2 right-1/2 -bottom-10 bg-white text-zinc-800 rounded-lg border-4 border-yellow-600'>
+        <form onSubmit={onSubmit} className='absolute  translate-x-1/2 right-1/2 -bottom-10 bg-white text-zinc-800 rounded-lg border-4 border-yellow-600'>
             <div className="flex justify-around items-center gap-5 py-2 px-12">
                 <div className='flex gap-2 items-center'>
                     <FontAwesomeIcon icon={faBed} className='text-gray-400 ' />
-                    <input type="text" placeholder={` Where are you going?`} />
+                    <input type="text" placeholder={` Where are you going?`} className='p-1 focus: outline-none focus:border-b focus:border-b-slate-500' />
                 </div>
                 <div className='flex gap-2 items-center'>
                     <FontAwesomeIcon icon={faCalendar} className='text-gray-400 ' />
-                    <input type="text" pattern="\d{4}-\d{2}-\d{2}" placeholder='06/24/2022 to 06/24/2024' />
+                    <input type="text" pattern="\d{4}-\d{2}-\d{2}" placeholder='2022-06-24 to 2022-06-24' className='p-1 focus: outline-none focus:border-b focus:border-b-slate-500' />
+                    {/* placeholder='06/24/2022 to 06/24/2024' */}
                 </div>
                 <div className='flex gap-2 items-center'>
                     <FontAwesomeIcon icon={faFemale} className='text-gray-400 ' />
-                    <input type="text" placeholder='1 adult - 0 children - 0 room' />
+                    <input type="text" placeholder='1 adult - 0 children - 0 room' className='p-1 focus: outline-none focus:border-b focus:border-b-slate-500' />
                 </div>
                 <div className='flex gap-2 items-center'>
                     <Button label='Search' className=' bg-blue-600 text-white py-3 px-3' />
