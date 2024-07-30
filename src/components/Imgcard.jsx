616dcdf71af4cb102c3e8a760fffcdbc8303edf1
tien <tienvn998@gmail.com>
2024-07-30 20:40:51 +0700

    ImgCard repair

--- src/components/Imgcard.jsx
+++ src/components/Imgcard.jsx
@@ -8,14 +8,18 @@ import React, { useState } from "react";
  */
 function Imgcard({ imgUrl, imgAlt, ...props }) {
     const [imgHeight, setImgHeight] = useState('0px');
+    let [isScale, setIsScale] = useState(false);
 
     const onSetImgHeight = e => {
-        setImgHeight(e.currentTarget.offsetWidth + 'px')
+        if (e.currentTarget.offsetWidth > 600)
+            setIsScale(true);
+        setImgHeight(e.currentTarget.parentElement.offsetWidth + 'px')
     };
 
     return (
-        <div className={`overflow-hidden relative w-full`} {...props} style={{ height: imgHeight }} >
-            <img src={imgUrl} alt={imgAlt} className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-max w-max' onLoad={onSetImgHeight} />
+        <div className={`overflow-hidden relative`} {...props} style={{ height: imgHeight }} >
+            <img src={imgUrl} alt={imgAlt} className='absolute top-1/2 left-1/2 max-w-max max-h-max' onLoad={onSetImgHeight} style={{ transform: `translate(-50%, -50%) ${isScale ? 'scale(.35)' : 'scale(.5)'}` }} />
+            {/* -translate-x-1/2 -translate-y-1/2 h-max w-max */}
         </div>
     );
 }
