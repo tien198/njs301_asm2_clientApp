4060e64fc1622289a39c0a00fc894dad756557e1
tien <tienvn998@gmail.com>
2024-07-31 08:30:21 +0700

    complete searchContents

--- src/pages/detail/Detail.jsx
+++ src/pages/detail/Detail.jsx
@@ -1,8 +1,10 @@
+import Navbar from '../../components/Navbar';
+
 const Detail = () => {
   return (
-    <div>
-      <h1>Detail Page</h1>
-    </div>
+    <>
+      <Navbar />
+    </>
   );
 };
 
--- src/pages/search/Search.jsx
+++ src/pages/search/Search.jsx
@@ -81,6 +81,17 @@ function SearchDiv() {
 }
 
 function SearchedItem({ item }) {
+  return (
+    <div className={`grid ${styles['grid-search-item']} items-center gap-4 p-3 rounded-lg border`}>
+      <ImgCard imgUrl={item.image_url} imgAlt={item.name} />
+      {/* <div style={{ backgroundImage: `url(${item.image_url})` }} className='bg-cover h-full w-full' ></div> */}
+      <SearchedItemSecondCol item={item} />
+      <SearchedItemThirdCol item={item} />
+    </div>
+  );
+}
+
+function SearchedItemSecondCol({ item }) {
   const freeCancel = item.free_cancel ?
     [
       <strong className='text-green-600' key={1}>Free cancellation</strong>,
@@ -88,16 +99,28 @@ function SearchedItem({ item }) {
     ] : null;
 
   return (
-    <div className={`grid ${styles['grid-search-item']} items-center gap-4 p-3 rounded-lg border`}>
-      <ImgCard imgUrl={item.image_url} imgAlt={item.name} />
-      {/* <div style={{ backgroundImage: `url(${item.image_url})` }} className='bg-cover h-full w-full' ></div> */}
-      <div className='flex flex-col h-full gap-1 justify-between'>
-        <h3 className='text-xl font-semibold lg:font-bold text-blue-600'>{item.name}</h3>
-        <p>{item.distance} from center</p>
-        <p><span className='bg-green-600 text-white rounded p-1'>{item.tag}</span></p>
-        <p className='font-semibold'>{item.description}</p>
-        <p>{item.type}</p>
-        {freeCancel}
+    <div className='flex flex-col h-full gap-1 justify-between'>
+      <h3 className='text-xl font-semibold lg:font-bold text-blue-600'>{item.name}</h3>
+      <p>{item.distance} from center</p>
+      <p><span className='bg-green-600 text-white rounded p-1'>{item.tag}</span></p>
+      <p className='font-semibold'>{item.description}</p>
+      <p>{item.type}</p>
+      {freeCancel}
+    </div>
+  );
+}
+
+function SearchedItemThirdCol({ item }) {
+  return (
+    <div className='flex flex-col justify-between h-full'>
+      <div className='flex justify-between'>
+        <span className='text-lg font-semibold'>{item.rate_text}</span>
+        <span className='bg-main-color text-white p-1'>{item.rate}</span>
+      </div>
+      <div className='flex flex-col items-end gap-2'>
+        <span className='text-3xl font-semibold'>$ {item.price}</span>
+        <span className='text-gray-500 text-sm'>Include taxes and fees</span>
+        <Button label='See availability' className='bg-blue-600 text-white hover:bg-blue-800 font-semibold rounded-md py-3 px-8' />
       </div>
     </div>
   );
