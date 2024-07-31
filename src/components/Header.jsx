791ca4ab66b89a4c8210ccfa46b8e93787ea4511
tien <tienvn998@gmail.com>
2024-07-31 16:40:47 +0700

    add react-date-range

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,12 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBed, faCalendar, faFemale } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
 
+// react-date-range
+import { DateRange } from 'react-date-range';
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { useState } from 'react';
+
 export default function Header() {
     return (
         <div className='bg-main-color text-white pt-10 pb-20 relative px-5 md:px-7'>
@@ -22,21 +28,50 @@ function SearchForm() {
     const onSubmit = e => {
         e.preventDefault();
     }
+
+    const [dateRangeHidden, setDateRangeHidden] = useState(false);
+    const [state, setState] = useState([
+        {
+            startDate: new Date(),
+            endDate: null,
+            key: 'selection'
+        }
+    ]);
+
+    const setDateHidden = e => {
+        setDateRangeHidden(prev => {
+            if (prev) return false;
+            return true;
+        })
+    }
+    const setDate = e => {
+        console.log(e.selection);
+        setState([e.selection])
+
+    }
+    const hiddenClass = dateRangeHidden ? 'hidden' : '';
+    // const hiddenClass = '';
     // const searchBtn = e => {
     //     window.location.replace('/search');
     // }
     return (
-        <form onSubmit={onSubmit} className='absolute  translate-x-1/2 right-1/3 -bottom-14 lg:right-1/2 lg:-bottom-10 bg-white text-zinc-800 rounded-lg border-4 border-yellow-600'>
+        <form onSubmit={onSubmit} className='absolute  translate-x-1/2 right-1/3 -bottom-14 lg:right-1/2 lg:-bottom-10 bg-white text-zinc-800 rounded-lg border-4 border-yellow-600 z-50'>
             <div className='flex justify-around items-center flex-wrap lg:flex-nowrap gap-5 py-2 px-12'>
                 <div className='flex gap-2 items-center'>
                     <FontAwesomeIcon icon={faBed} className='text-gray-400 ' />
                     <input type='text' placeholder={` Where are you going?`} className='p-1 focus: outline-none focus:border-b focus:border-b-slate-500' />
                 </div>
-                <div className='flex gap-2 items-center'>
+                <div className='flex gap-2 items-center relative z-50' onClick={setDateHidden}>
                     <FontAwesomeIcon icon={faCalendar} className='text-gray-400 ' />
-                    <input type='text' pattern='\d{4}-\d{2}-\d{2}' placeholder='2022-06-24 to 2022-06-24' className='p-1 focus: outline-none focus:border-b focus:border-b-slate-500' />
+                    <input type='text' pattern='\d{4}-\d{2}-\d{2}' placeholder='2022-06-24 to 2022-06-24' className='p-1 focus: outline-none focus:border-b focus:border-b-slate-500 relative   ' />
                     {/* placeholder='06/24/2022 to 06/24/2024' */}
                 </div>
+                <DateRange
+                    editableDateInputs={true}
+                    onChange={setDate}
+                    moveRangeOnFirstSelection={false}
+                    ranges={state}
+                    className={`absolute top-12  ${hiddenClass}`} minDate={new Date(2022, 1, 1)} maxDate={new Date(Date.now())} />
                 <div className='flex gap-2 items-center'>
                     <FontAwesomeIcon icon={faFemale} className='text-gray-400 ' />
                     <input type='text' placeholder='1 adult - 0 children - 0 room' className='p-1 focus: outline-none focus:border-b focus:border-b-slate-500' />
