928487bf31740b49a96fa350612e780a86d0475c
tien998 <tienvn998@gmai.com>
2025-04-23 16:23:20 +0700

    init /pages/hotelManagement/

--- src/pages/home/comps/Hotel.jsx
+++ src/pages/home/comps/Hotel.jsx
@@ -5,9 +5,9 @@ function Hotel({ hotel }) {
             <div style={{ backgroundImage: `url(${hotel.photos[0]})` }} className='mb-4 w-full h-56 lg:h-96 bg-cover bg-center'></div>
             <div className='flex flex-col gap-2'>
                 <p className='font-semibold underline text-violet-600'>{hotel.name}</p>
-                <p className='font-light'>{hotel.city} hotels</p>
-                <p className='font-semibold'>Starting from ${hotel.price}</p>
-                <p><span className='text-white bg-blue-800 p-1'>{hotel.rate}</span> {hotel.type}</p>
+                <p className='font-light'>{hotel.type}</p>
+                <p className='font-semibold'>Starting from ${hotel.cheapestPrice}</p>
+                <p><span className='text-white bg-blue-800 p-1'>{hotel.rating}</span> {hotel.type}</p>
             </div>
         </div>
     )
